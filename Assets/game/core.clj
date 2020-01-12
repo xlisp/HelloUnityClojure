ce91fb0a66476cc13b517a1fea967d3c5d801010
chanshunli <chanshunli@gmail.com>
2020-01-12 17:35:07 +0800

    _

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -4,7 +4,7 @@
   (:import [UnityEngine Vector3]))
 ;; `C-c C-k`就可以生成core.clj.meta : 每个资源文件（外部导入和内部创建的）对应一个.meta文件。这个.meta文件中的guid就唯一标识这个资源。
 
-(defonce c (create-primitive :cube))
+(defonce c1 (create-primitive :cube))
 
 (defn move!
   [go v3]
@@ -12,4 +12,4 @@
         (l/v3+ (.. go transform position)
                v3)))
 
-;;(move! c (Vector3. 2 0 0)) ;; 在Scene页面上面移动成功了
+;;(move! c1 (Vector3. 2 0 0)) ;; 在Scene页面上面移动成功了
